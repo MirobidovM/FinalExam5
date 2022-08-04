0b7f0daf25af8f15223f270b5118d43a2a2edfc0
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-08-04 22:39:35 +0500

    initial commit

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,5 +3,6 @@ import { AuthContext } from '../context/auth';
 
 export const useAuth = (props) => {
   const { token, setToken } = useContext(AuthContext);
+
   return props ? [setToken] : [token, setToken];
 };
