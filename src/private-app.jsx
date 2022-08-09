8d7dd091b323ba7e40e6f358614b22b415b7fe24
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-08-09 21:00:30 +0500

    initial commit

--- src/private-app.jsx
+++ src/private-app.jsx
@@ -23,6 +23,7 @@ export const PrivateApp = () => {
               <Header />
               <Routes>
                 <Route path="/" element={<All />} />
+                
                 <Route path="/all" element={<All />} />
                 <Route path="/design" element={<Design />} />
                 <Route path="/ux" element={<Ux />} />
